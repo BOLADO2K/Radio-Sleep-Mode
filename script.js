4b87661c653e69f20e85247db7c1efcb2c471203
BOLADO2K <111667286+BOLADO2K@users.noreply.github.com>
2023-02-27 12:32:23 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+/* script- para mudar fundo da pagina */
+
+// Seleciona o botão e adiciona um ouvinte de evento para o evento "click"
+const button = document.getElementById("change-background-button");
+button.addEventListener("click", function() {
+  // Seleciona o elemento "body" e adiciona ou remove a classe "background2"
+  document.body.classList.toggle("background2");
+  
+});
+/* script- para mudar fundo da pagina */
+
+
+
+
+
+/* audio */
+
+  const audio = document.getElementById("audio");
+  const playpause = document.getElementById("playpause");
+  const volume = document.getElementById("volume");
+
+  function togglePlayPause() {
+    if (audio.paused) {
+      audio.play();
+      playpause.innerHTML = "Pause";
+    } else {
+      audio.pause();
+      playpause.innerHTML = "Play";
+    }
+  }
+
+  function setVolume() {
+    audio.volume = volume.value;
+  }
+
+  playpause.addEventListener("click", togglePlayPause);
+  volume.addEventListener("input", setVolume);
+
+
+/* audio */
+
+
+
+
+
+
